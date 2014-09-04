58d7c3f4570835ac622337f98a6ea9d0722ca19e
Yunsheng Wei <weiyunshengwei@gmail.com>
2014-09-04 17:57:52 +0800

    End of a day

---
+++ time_sequence.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 04 16:03:51 2014
+
+@author: WEIYUNSHENG
+"""
+
+def Time_Sequence:
+    """
+    This class is used to simplify representation.
+    """
+    
+    def __init__(self, l):
+        """
+        l is a list of ordered time.
+        time should be integer, timestamp
+        """
+        self.time_sequence = l
